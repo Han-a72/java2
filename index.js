36868e7c68c73cfb1cb7c091714139650747826d
Hana <hanaaman921@gmial.com>
2024-10-08 13:08:33 +0300

    first commit on js

---
+++ index.js
@@ -0,0 +1,27 @@
+function sumOfTheNumber(a,b){
+if(a===b){
+
+    return(a+b)*3;
+}
+else(a<=b)
+{
+    return(a+b);
+}
+}
+console.log(sumOfTheNumber(8,8));
+//part2
+function rangeNo(num1,num2){
+ return(num1 >=50 && num2<=99)
+  
+}
+console.log(rangeNo(99,40))
+//part3
+function closestNo(no1)
+{
+  return( no1*100)
+}
+console.log(closestNo(7.9))
+
+expression1 ="The sum of 1+1 =1+1"
+
+expression2=`The sum of 1+1 = ${1+1}`
