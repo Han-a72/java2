c04ee61d1aceebc75c18d91972c60bc8776ebf41
Hana <hanaaman921@gmial.com>
2024-10-09 13:00:34 +0300

    first commit on js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ else(a<=b)
     return(a+b);
 }
 }
-console.log(sumOfTheNumber(8,8));
+console.log(sumOfTheNumber(7,8));
 //part2
 function rangeNo(num1,num2){
  return(num1 >=50 && num2<=99)
@@ -16,12 +16,34 @@ function rangeNo(num1,num2){
 }
 console.log(rangeNo(99,40))
 //part3
-function closestNo(no1)
+function closestNo(c,d)
 {
-  return( no1*100)
+
+ let num1=100-c;
+ let num2=100-d;
+ if (num1<num2){
+  return(num2)
+ }
+else (nmu2<num1)
+{
+  return(num1)
 }
-console.log(closestNo(7.9))
 
-expression1 ="The sum of 1+1 =1+1"
+}
+console.log(closestNo(70,30))
 
-expression2=`The sum of 1+1 = ${1+1}`
+function checkNearset(x,y){
+const num1 =Math.abs(100-x) //70
+const num2 =Math.abs (100-y) // 100
+if (num1>num2){
+
+    
+  return y
+}
+else
+{
+  return x // 30
+}
+
+}
+console.log(checkNearset(30,200))
